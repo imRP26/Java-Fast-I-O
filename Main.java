0bfa4ace694c8d159422742c98e5a927e890ffa4
Rahul Padhy <104157969+JimHalpert26@users.noreply.github.com>
2022-12-03 22:49:01 +0530

    Fast Java I/O

---
+++ Main.java
@@ -0,0 +1,70 @@
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+
+public class Main {
+
+	//PrintWriter for faster output
+	public static PrintWriter out;
+
+	//MyScanner class for faster input
+	public static class MyScanner {
+		BufferedReader br;
+		StringTokenizer st;
+
+		public MyScanner() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+
+		String next() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				}
+				catch (IOException as e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+
+		long nextLong() {
+			return Integer.parseLong(next());
+		}
+
+		double nextDouble() {
+			return Double.parseDouble(next());
+		}
+
+		String nextLine() {
+			String str = "";
+			try {
+				str = br.readLine();
+			}
+			catch (IoException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+	}
+
+	public static void main(String[] args) {
+		MyScanner sc = new MyScanner();
+		out = new PrintWriter(new BufferedOutputStream(System.out));
+		//starting the solution below...
+		int n = sc.nextInt(); //reading input as an integer
+		long k = sc.nextLong(); //reading input as long
+		double d = sc.nextDouble(); //reading input as double
+		String str = sc.next(); //reading input as String
+		String s = sc.nextLine(); //reading whole line as string
+		int result = 3 * n;
+		out.println(result); //print via PrintWriter
+		//finishing the solution code here...
+		out.close(); 
+	}
+}
