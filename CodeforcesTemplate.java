075f9312fd419c83f064dd7dbce48c384717fc26
Rahul Padhy <104157969+JimHalpert26@users.noreply.github.com>
2022-12-10 12:50:48 +0530

    Java Fast I/O for CF
    Verified after submitting for the problem 1363C!!

---
+++ CodeforcesTemplate.java
@@ -0,0 +1,71 @@
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+
+public class Codeforces {
+
+	public static PrintWriter out; //PrintWriter for faster output
+
+	public static class FastScanner { //MyScanner class for faster input
+		BufferedReader br;
+		StringTokenizer st;
+
+		public FastScanner() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+		
+		public FastScanner(String s) {
+		    try {
+		        br = new BufferedReader(new FileReader(s));    
+		    }
+		    catch (FileNotFoundException e) {
+		        e.printStackTrace();
+		    }
+		}
+
+		String next() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				}
+				catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+
+		double nextDouble() {
+			return Double.parseDouble(next());
+		}
+
+		String nextLine() {
+			String str = "";
+			try {
+				str = br.readLine();
+			}
+			catch (IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+	}
+
+	public static void main(String[] args) throws java.lang.Exception {
+		FastScanner sc = new FastScanner();
+		out = new PrintWriter(new BufferedOutputStream(System.out));
+		//starting the solution below...
+		
+    //finishing the solution code here...
+		out.close(); 
+	}
+}
